0e67929739b458524a106b64b25e5744cdb08631
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-05-14 22:51:15 +0200

    Bumped version to 1.1.2

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@ if (!defined("IN_ESOTALK")) exit;
 ET::$pluginInfo["Signature"] = array(
 	"name" => "Signature",
 	"description" => "Let users add their signature to posts.",
-	"version" => "1.1.1",
+	"version" => "1.1.2",
 	"author" => "Tristan van Bokkem",
 	"authorEmail" => "tristanvanbokkem@gmail.com",
 	"authorURL" => "http://www.bitcoinclub.nl",
